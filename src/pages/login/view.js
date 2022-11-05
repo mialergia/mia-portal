1d7bb857d3a567e331e32e7df8e132989650a390
Alvaro Badaracco <alvaro.badaracco@mercadolibre.com>
2022-11-05 18:38:32 -0300

    Added login styles

--- src/pages/login/view.js
+++ src/pages/login/view.js
@@ -1,6 +1,7 @@
 const React = require("react");
 const Head = require("react-declarative-head");
 const Icon = require("../../components/assets/appIcon.png");
+const Background = require("../../components/assets/mialergia-fondo.jpeg");
 
 const { Button, Form } = require("react-bootstrap");
 
@@ -11,21 +12,28 @@ const View = () => {
         <title>MiaPortal | LOGIN</title>
         <link rel="icon" href={Icon}></link>
       </Head>
-      <img src={Icon}></img>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
-        </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Ingresar
-        </Button>
-      </Form>
+      <div className="login__container">
+        <img className="login__background-image" src={Background}></img>
+        <div className="login__right-container">
+          <img src={Icon}></img>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Ingresar
+            </Button>
+          </Form>
+        </div>
+      </div>
+
     </div>
   );
 };
